241da4e43702aa72ae29b47214cffba196ad400b
CarlosGerardo23 <carlos.enriquez.valerio@gmail.com>
2020-02-01 18:16:21 -0600

    IA y un super script del editor

--- Assets/Carlos/Scripts/Characters/Path.cs
+++ Assets/Carlos/Scripts/Characters/Path.cs
@@ -7,20 +7,12 @@ public class Path : MonoBehaviour
 
     public GameObject parentWayPoint;
 
-    public List<Transform> wayPoints;
-    [SerializeField]float speed;
-    [SerializeField]float limitDistance;
+    public List<SpecialWayPoints> wayPoints;
+    [SerializeField] float speed;
+    [SerializeField] float limitDistance;
     private int currentPoint;
+    private bool canStop;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        wayPoints= new List<Transform>();
-        for(int i=0; i<parentWayPoint.transform.childCount;i++)
-        {
-            wayPoints.Add(parentWayPoint.transform.GetChild(i));
-        }
-    }
 
     // Update is called once per frame
     void Update()
@@ -30,14 +22,54 @@ public class Path : MonoBehaviour
 
     private void Move()
     {
-        if(currentPoint>wayPoints.Count-1)
-        return;
+        if (currentPoint > wayPoints.Count - 1)
+        {
+            currentPoint = 0;
+            return;
+        }
+        canStop = wayPoints[currentPoint].stop;
+        Vector3 vector = wayPoints[currentPoint].points.position - transform.position;
 
-        Vector3 vector= wayPoints[currentPoint].position-transform.position;
-        transform.Translate(vector.normalized*speed*Time.deltaTime,Space.World);
+        transform.Translate(vector.normalized * speed * Time.deltaTime, Space.World);
 
-        if(Vector3.Distance(transform.position,wayPoints[currentPoint].position)<limitDistance)
+        if (Vector3.Distance(transform.position, wayPoints[currentPoint].points.position) < limitDistance)
+        {
+            if (canStop)
+            {
+                canStop = !wayPoints[currentPoint].CheckStopTime();
+                
+                if (canStop)
+                    return;
+            }
             currentPoint++;
-        
+        }
+
+
+    }
+}
+
+[System.Serializable]
+public class SpecialWayPoints
+{
+    public bool stop;
+
+    public float time = 6;
+    public Transform points;
+    private float currentTime;
+
+    public SpecialWayPoints()
+    {
+        currentTime = time;
+    }
+    public bool CheckStopTime()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            currentTime = time;
+            return true;
+        }
+
+        return false;
     }
 }
