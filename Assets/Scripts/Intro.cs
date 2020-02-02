cc3eb1a472e5ee1babc9dafe3ddefa299d846e43
PamMontiel <60006288+PamMontiel@users.noreply.github.com>
2020-02-02 10:40:50 -0600

    Intro con imagenes y botón

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Sprite[] story;
+	private Image frame;
+	private int frameCounter = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	IEnumerator playerStory()
+	{
+		if (frameCounter < story.Length)
+		{
+			frame.sprite = story[frameCounter];
+			yield return new WaitForSeconds(5);
+			frameCounter++;
+			StartCoroutine(playerStory());
+		}
+		else
+		{
+			SceneManager.LoadScene("Menu");
+		}
+
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+		frame = GetComponent<Image>();
+		StartCoroutine(playerStory());
+	}
+
 }
